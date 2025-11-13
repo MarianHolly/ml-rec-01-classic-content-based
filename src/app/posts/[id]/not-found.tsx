7c371021d921a556f7ac7a075bb528ed43699401
Marián Hollý <marian.holly7117@gmail.com>
2025-11-13 20:46:49 +0100

    github reset

--- src/app/posts/[id]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="text-center my-24">
-      <h1 className="text-3xl font-medium">No post found for that id.</h1>
-    </div>
-  );
-}
--- src/app/posts/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function loading() {
-  return (
-    <div className="text-center my-24">
-      <h1 className="text-3xl font-extralight">Loading</h1>
-    </div>
-  );
-}
